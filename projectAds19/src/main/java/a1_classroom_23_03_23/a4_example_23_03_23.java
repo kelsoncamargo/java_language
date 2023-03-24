eaf5c48b118415d13e7fa9b8e58feb53e1b73a46
Kelson Machado Camargo <kelson.m.camargo@hotmail.com>
2023-03-23 21:46:03 -0300

    feat: first classroom day 23/03/23 second commit

--- projectAds19/src/main/java/a1_classroom_23_03_23/a4_example_23_03_23.java
+++ projectAds19/src/main/java/a1_classroom_23_03_23/a4_example_23_03_23.java
@@ -1,13 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package a1_classroom_23_03_23;
 
-/**
- *
- * @author kelso
- */
 public class a4_example_23_03_23 {
-    
+  public static void main(String args[]){
+  
+      String myName = "Kelson";
+      String lastName = "Camargo";
+      String completName = myName + " " + lastName;
+      System.out.println("O nome completo do usuário é: " + completName);
+      
+  }  
 }
---
+++ projectAds19/src/main/java/a1_classroom_23_03_23/a5_exercise_23_03_23.java
@@ -0,0 +1,12 @@
+package a1_classroom_23_03_23;
+
+public class a5_exercise_23_03_23 {
+    public static void main(String args[]){
+        
+        int squareLenght = 10;
+        int resultAreaSquare = squareLenght * squareLenght;
+        System.out.println("O resultado da área do quadrado é: " + resultAreaSquare);
+   
+    }
+    
+}
